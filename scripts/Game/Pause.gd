31ad682c99064cc1cb813891f6fa1547312523ac
Goncalo Amaral <goa.docs@gmail.com>
2020-07-14 23:08:10 +0100

    Pause menu

--- scripts/Game/Pause.gd
+++ scripts/Game/Pause.gd
@@ -1,6 +1,12 @@
-extends Control
+extends CanvasLayer
+
+onready var menu_node: Control = $"./Menu"
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		get_tree().paused = not get_tree().paused
-		visible = not visible
+		get_tree().paused = true
+		menu_node.visible = true
+
+func _on_ContinueButton_pressed():
+	get_tree().paused = false
+	menu_node.visible = false

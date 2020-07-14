39bcb905081bcc3c64458caaed0f12357dbcfdd2
Goncalo Amaral <goa.docs@gmail.com>
2020-07-14 22:18:04 +0100

    Improve scene changer animation fade in/out

--- scripts/SceneChanger.gd
+++ scripts/SceneChanger.gd
@@ -4,11 +4,10 @@ signal scene_changed()
 
 onready var animation_player = $AnimationPlayer
 
-func change_scene(scene_name, delay = 0.5):
-	yield(get_tree().create_timer(delay), "timeout")
+func change_scene(scene_name):
 	animation_player.play("fade_in")
 	yield(animation_player, "animation_finished")
 	assert(get_tree().change_scene("scenes/%s.tscn" % scene_name) == OK)
-	animation_player.play("fade_out")
+	animation_player.play_backwards("fade_in")
 	yield(animation_player, "animation_finished")
 	emit_signal("scene_changed")
